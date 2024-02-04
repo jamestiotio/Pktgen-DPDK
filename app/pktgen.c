252069ab571601e8454580f0c2aa8f2dff284ad9
Keith Wiles <keith.wiles@intel.com>
2024-02-04 10:06:20 -0600

    fix build issue after merge
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

--- app/pktgen.c
+++ app/pktgen.c
@@ -263,7 +263,6 @@ pktgen_find_matching_ipdst(port_info_t *pinfo, uint32_t addr)
 static __inline__ tstamp_t *
 pktgen_tstamp_pointer(port_info_t *pinfo __rte_unused, char *p)
 {
-    (void)pinfo;
     int offset = 0;
 
     offset += sizeof(struct rte_ether_hdr);
@@ -1040,10 +1039,6 @@ pktgen_main_transmit(port_info_t *pinfo, uint16_t qid)
 
         pktgen_setup_packets(pinfo->pid);
 
-        if (pktgen_tst_port_flags(pinfo, SEND_PCAP_PKTS))
-            mp = l2p_get_pcap_mp(pinfo->pid);
-
-=======
         pinfo->qcnt[qid]++; /* Count the number of times queue is sending */
 
         if (pktgen_tst_port_flags(pinfo, SEND_PCAP_PKTS))
